89034b0ab77209b760aa53268f068a9e1e580df0
neuroevolutus <sentabel@gmail.com>
2018-07-14 23:03:00 -0500

    Update gitignore and add lines to retrieve list of tables in database

--- test.py
+++ test.py
@@ -2,12 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
 if __name__ == "__main__":
+
     engine = create_engine('mssql+pymssql://inventorymgmt:angel10!@den1.mssql5.gear.host:1433/inventorymgmt')
     connection = engine.connect()
+
     s = text(
         "SELECT * FROM VOLUNTEER"
     )
+    s = text(
+        "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'"
+    )
+
     result = connection.execute(s).fetchall()
-    for item in result:
-        print(item)
+    table_names = [item[0] for item in result]
+    print(table_names)
+
     connection.close()

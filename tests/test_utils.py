0b67340a0a1c1c9c1aed9012fe779d9510ff93ee
neuroevolutus <sentabel@gmail.com>
2018-07-22 15:22:50 -0500

    Add tests

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("..")
+from tests.util_methods import *
+
+def make_English_list():
+    assert format_into_English_list(["Abel"]) == "Abel"
+    assert format_into_English_list(["Abel", "Andrew"]) == "Abel and Andrew"
+    assert format_into_English_list(["Abel", "Andrew", "Beerus"]) == "Abel, Andrew and Beerus"
+    assert format_into_English_list(["Abel", "Andrew", "Beerus", "Vegita"]) == "Abel, Andrew, Beerus and Vegita"
